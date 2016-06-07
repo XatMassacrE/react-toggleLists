f2425249b5b05e35b223fdbb00f44f82d53b3e4a
Xat_MassacrE <ywt13639397661@gmail.com>
2016-06-08 02:38:47 +0800

    Add <DepTRow, PostRow, PostTabel> in one file to generate basic page without stylesheet

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,7 +2,7 @@
 import React, {Component, PropTypes} from 'react';
 import ReactDOM from 'react-dom';
 
-import ShowList from './ShowList.jsx';
+// import ShowList from './ShowList.jsx';
 // import MainContent from './MainContent.jsx';
 // import Title from './Title.jsx';
 // import RD_Dept from './RD_Dept.jsx';
@@ -27,9 +27,70 @@ class App extends Component {
       // <section className = "vertical-nav">
       //   //<DeptNav />
       // </section>
-      <section className = "content-page xat-badge">
-        hello 你好我也好
-      </section>
+      // <section className = "content-page xat-badge">
+      //   hello 你好我也好
+      // </section>
+      <div>
+        <PostTabel postData={listData}/>
+      </div>
+    );
+  }
+}
+class DepTRow extends Component {
+  render() {
+    return (
+      <tr>
+        <th>
+          <input type='checkbox' />
+        </th>
+        <th style={{border: '1px solid'}}>
+          {this.props.category}
+        </th>
+        <th className='xat-badge'>
+          120
+        </th>
+      </tr>
+    );
+  }
+}
+class PostRow extends Component {
+  render() {
+    var name = this.props.postData.name;
+    return (
+      <tr>
+        <th>
+          <input type='checkbox' />
+        </th>
+        <td>{name}</td>
+        <td>{this.props.postData.count}</td>
+      </tr>
+    );
+  }
+}
+class PostTabel extends Component {
+  render() {
+    var rows = [];
+    var lastCategory = null;
+    this.props.postData.map((post) => {
+      console.log(post.category !== lastCategory);
+      if (post.category !== lastCategory) {
+        rows.push(<DepTRow category={post.category} key={post.category} />);
+      }
+      rows.push(<PostRow postData={post} key={post.name} />);
+      lastCategory = post.category;
+    });
+    return (
+      <table>
+        <thead>
+          <tr>
+            <th>招聘职位</th>
+            <th>清空</th>
+          </tr>
+        </thead>
+        <tbody>
+          {rows}
+        </tbody>
+      </table>
     );
   }
 }
